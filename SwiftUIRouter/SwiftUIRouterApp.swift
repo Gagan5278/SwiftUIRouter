5fefa17bb7fab64c01aa0316f600f519cc47ea35
Gagan Vishal Mishra <gagan.vishal@mmltd.co.za>
2024-01-28 12:55:29 +0100

    Initial Commit

---
+++ SwiftUIRouter/SwiftUIRouterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIRouterApp.swift
+//  SwiftUIRouter
+//
+//  Created by Gagan Vishal  on 2024/01/28.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIRouterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
